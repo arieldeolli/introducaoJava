67354406a72dfdaeded4d0173cad886e2f16b03f
Ariel Vieira de Oliveira <5671060@univali.br>
2016-08-23 12:21:28 -0300

    simulando correção de problema

--- src/Principal.java
+++ src/Principal.java
@@ -11,6 +11,6 @@
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Primeiro código");
+        System.out.println("Primeiro códigoA");
     }
 }
